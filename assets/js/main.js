144b9d6ecbf0c63bd156eee6ced26b09f6a68d04
LorenzoMegna24 <megnalorenzo0@gmial.com>
2023-04-14 15:49:38 +0200

    Snack 1 array oggetti

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,44 @@ Snack 1
 Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+*/
+const bici = [
+  {
+    nome:"Wilier",
+    peso: 20
+  },
+  {
+    nome: "Scott",
+    peso: 23
+  },
+  {
+    nome: "Bianchi",
+    peso: 19
+  },
+  {
+    nome: "Olmo",
+    peso: 25
+  }
+]
+
+console.log(bici);
+
+
+
+
+
+
+
+
+/*
 Snack2
 Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà:
 Punti fatti e falli subiti.
 Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+*/
+/*
 Snack 3
 Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 l'array egnerato da parte vostra dovrà essere di ad esempio nomi o cose: ['alberto', 'michele','angelo','simone']
